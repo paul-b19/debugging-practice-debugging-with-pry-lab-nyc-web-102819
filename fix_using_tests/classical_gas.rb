3ef31fd59f079b2b7e0b199c64815595fd988769
Pavel Beletsky <pavel.beletsky@live.com>
2019-10-26 05:51:30 +0000

    Automatically backed up by Learn

--- fix_using_tests/classical_gas.rb
+++ fix_using_tests/classical_gas.rb
@@ -1,9 +1,8 @@
 class GasStation
 
-  attr_reader :brand, :unleaded_price
+  attr_reader :brand :unleaded_price
 
-  #def initialize(brand, unleaded_price)
-  def dinobones(brand, unleaded_price)
+  def initialize(brand, unleaded_price)
     @brand = brand
     @unleaded_price = unleaded_price
   end
